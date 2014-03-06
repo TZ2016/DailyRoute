565e741dfaddb6b0f0e6d219882775edf36fe418
Qi <gzzhongqi@gmail.com>
2014-03-05 20:41:10 -0800

    added calcroute

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,10 @@
 var geocoder;
 var map;
+var rendererOptions = {
+  draggable: true
+};
+var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+var directionsService = new google.maps.DirectionsService();
 var markers = [];
 
 function initialize() {
@@ -8,13 +13,10 @@ function initialize() {
   var mapOptions = {
     zoom: 8,
     center: latlng
-  };
+  }
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  // event listerns
-  // google.maps.event.addListener(map, 'click', function(event) {
-  //   addMarker(event.latLng);
-  // });
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById("directionsPanel"));
 }
 
 function addMarker(loc, msg) {
@@ -73,5 +75,71 @@ function getNameOfAddress(geores) {
 //   });
 //   return components.join(", ");
 // }
+
+
+// ==================================
+
+
+function calcRoute(data) {
+  console.log("++++++++++++");
+  var size = data.length
+  var start = new google.maps.LatLng(data[0].Lat, data[0].Long);
+  var end = new google.maps.LatLng(data[size-1].Lat, data[size-1].Long);
+  // var selectedMode = document.getElementById("mode").value;
+  var selectedMode = "DRIVING";
+  var waypts = [];
+  var loc;
+  for (var i = 1; i < size-1; i++) {
+    loc = new google.maps.LatLng(data[i].Lat, data[i].Long)
+    waypts.push({
+        location: loc,
+        stopover: true
+    });
+  }
+  var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode[selectedMode],
+    optimizeWaypoints: false,
+    waypoints: waypts
+  };
+
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      var route = response.routes[0];
+      var summaryPanel = document.getElementById("directions_panel");
+      summaryPanel.innerHTML = "";
+      // For each route, display summary information.
+      for (var i = 0; i < route.legs.length; i++) {
+        var routeSegment = i+1;
+        summaryPanel.innerHTML += "<b>Route Segment: " + routeSegment + "</b><br />";
+        summaryPanel.innerHTML += route.legs[i].start_address + " to ";
+        summaryPanel.innerHTML += route.legs[i].end_address + "<br />";
+        summaryPanel.innerHTML += route.legs[i].distance.text + "<br /><br />";
+      }
+    }
+  });
+
+  google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
+      computeTotalDistance(directionsDisplay.directions);
+  });
+}
+
+
+function computeTotalDistance(result) {
+  var total = 0;
+  var myroute = result.routes[0];
+  for (i = 0; i < myroute.legs.length; i++) {
+    total += myroute.legs[i].distance.value;
+  }
+  total = total / 1000.
+  document.getElementById("total").innerHTML = total + " km";
+}
+
+
+
+
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
