5ce0e2115b18a866a4ec15a1f4e23742c2d4fa71
hitocook <fangcai@berkeley.edu>
2014-03-13 23:23:05 -0700

    update solve.rb

--- app/controllers/solve.rb
+++ app/controllers/solve.rb
@@ -1,11 +1,12 @@
 # start and end are Location. locations is an Array of Location.
-def solve (start,locations,end)
-	all_loc = [start]+locations+[end]
-	result = request_distance(all_loc)
-	if result[:status] != 'ok'
-		render _____________
-		return
-	end
+ERR_REQUEST_FAIL = -1
+def solve (start,locations, dest)
+	all_loc = [start]+locations+[dest]
+	# result = request_distance(all_loc)
+	# if result[:status] != 'ok'
+	# 	render _____________
+	# 	return
+	# end
 	info = result.rows
 	arranged,unarranged = classify_loc(all_loc)
 	if arranged == []
@@ -13,14 +14,34 @@ def solve (start,locations,end)
 	else
 		return fit_schedule(arranged, unarranged)
 	end
-
-def shortest_path(location,info)
-	pass
-
+end
+## http://maps.googleapis.com/maps/api/directions/json?origin=Adelaide,SA&destination=Adelaide,SA&waypoints=optimize:true|Barossa+Valley,SA|Clare,SA|Connawarra,SA|McLaren+Vale,SA&sensor=false&key=API_KEY
+def shortest_path(start, locations, dest)
+	result = request_route(start,location, dest)
+	if result[:status] != 'OK':
+		render :json => {errCode: ERR_REQUEST_FAIL}
+	ren
+end
 
 def fit_schedule(arranged, unarranged,info)
 	pass
+end
 
+def request_route(start, locations, dest)
+	addr = 'http://maps.googleapis.com/maps/api/directions/json?'
+	origin= 'origin=%s' % start.gecode
+	dest = 'destination=%s&' % dest.gecode
+	places = ''
+	locations.each do |point|
+		places = point.gecode+'|'
+	end
+	places[places.length-1]='&'
+	passby ='waypoints=optimize:true|%ssensor=false'%places
+	addr = addr+origin+dest+passby
+	require 'net/http'
+	return Net::HTTP.get(URI.parse(addr))
+	
+end
 ## requires start.departafter and end.startbefore
 def classify_loc(locations)
 	arranged,unarranged = [],[]
@@ -36,12 +57,14 @@ def classify_loc(locations)
 	end
 	_______!!!validity____________
 	return arranged, unarranged
+end
 def request_distance(locations)
 	address = 'http://maps.googleapis.com/maps/api/distancematrix/json?'
-	place = ''
+	places = ''
 	locations.each do |point|
 		places = point.gecode+'|'
-	places[places.length]='&'
+	end
+	places[places.length-1]='&'
 	origins = 'origins='+places
 	destination = 'destinations='+places
 	mode = 'mode=' + @route.travelMethod
@@ -49,6 +72,7 @@ def request_distance(locations)
 	# http://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&language=fr-FR&sensor=false&key=API_KEY
 	require 'net/http'
 	return Net::HTTP.get(URI.parse(address))
+end
 def preprocess(point)
 	if point.arrivebefore and point.departafter:
 		return
@@ -59,4 +83,5 @@ def preprocess(point)
 		point.arrivebefore = point.departbefore - point.minduration
 	end
 	point.save
+end
 
