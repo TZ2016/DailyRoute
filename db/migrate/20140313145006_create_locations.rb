e5ef5ba9aa66bbed464c17705161b9e62329267e
RichieChen <richiechen@berkeley.edu>
2014-03-14 00:29:18 -0700

    added geocode hash

--- db/migrate/20140313145006_create_locations.rb
+++ db/migrate/20140313145006_create_locations.rb
@@ -14,6 +14,7 @@ class CreateLocations < ActiveRecord::Migration
       t.integer :priority
       t.boolean :blacklisted
       t.boolean :lockedin
+      t.text :geocode
 
       t.timestamps
     end
