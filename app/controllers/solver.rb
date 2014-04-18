e44d860799778e40cdf5940bfd81be0828102601
hitocook <fangcai@berkeley.edu>
2014-04-18 16:07:08 -0700

    two location query works

--- app/controllers/solver.rb
+++ app/controllers/solver.rb
@@ -1,4 +1,5 @@
-module RoutesHelper
+module Solver
+
   require 'rubygems'
   require 'json'
   require 'net/http'
@@ -7,7 +8,7 @@ module RoutesHelper
   ERR_REQUEST_FAIL = -1
   ERR_INVALID_INPUT_TIME = -2
   ERR_NOT_ENOUGH_TIME_FOR_TRAVEL = -3
-  ERR_IN_SPECIFY_START_TIME_AND_ARRIVE_TIME = -4
+  ERR_NEED_SPECIFY_START_TIME_AND_ARRIVE_TIME = -4
   APP_KEY = "AIzaSyDjxIMvftYWM2uDN5s5GvFSODrFs2tRWEM"
 
   # Use information in @inp. Return a hash includes 
@@ -18,36 +19,24 @@ module RoutesHelper
     init(inp) 
     if @err != SUCCESS
       pp ' ========Error in classify=============== '
-  		solution = {errCode: @err}
-    elsif @fuzzy.empty? and @arranged.length == 2
-      solution = shortest_path(@inp['locationList'])
+  		return {errCode: @err}
+    elsif @fuzzy.empty? and @arranged.empty?
+      return shortest_path(@inp['locationList'])
     elsif @fuzzy.empty?
-      solution = fit_schedule
+      return fit_schedule
     else
-      solution = general_search
+      return general_search
     end
-    
-    pp '===================formatted solution======================'
-    format(solution)
-    pp solution
-    return solution
   end
 
-  #Change the time representation in the sol from Time to DateTime.
-  def format(sol)
-    if sol[:errCode] == SUCCESS
-      for route in sol[:routes]
-        for step in route[:steps]
-          step[:departure] = step[:departure].to_datetime
-          step[:arrival] = step[:arrival].to_datetime
-        end
-      end
-    end
-  end
+  # Clear all instance variables. Called in the beginning of any solve.
   
+
+
+
   def init(inp)
     @inp = inp
-    @start, @dest, @mode, @arranged, @unarranged, @fuzzy, @intervals = nil
+    @start, @dest, @mode, @arranged, @unarranged, @intervals = nil
     @year = DateTime.now.year
     @month = DateTime.now.month
     @day = DateTime.now.day
@@ -71,31 +60,33 @@ module RoutesHelper
   # go through all locs in passby.
   def shortest_path(locs)
     pp '=========in shortest path========='
+    pp @start
+    pp @dest
+    pp locations
     
     result = JSON.parse(request_route(locs))
     if result.has_key? 'Error' or result['status'] != 'OK'
       return {errCode: ERR_REQUEST_FAIL}
     end
-    legs = result['routes'][0]["legs"]
-    route1 = {steps:[], mode:@mode, name:'route'}
+    legs = result["legs"]
+    routes = []
     first_step = {}
-    # pp legs
-    first_step[:geocode] = geocode_to_s(legs[0]["start_location"])
-    first_step[:departure] = locs[0]['departafter']
-    first_step[:arrival] = first_step[:departure] + legs[0]["duration"]["value"]
-    route1[:steps]<< first_step
-    for leg in legs
+    first_step[:geocode] = geocode_to_s(p1['geocode'])
+    fisrt_step[:departtime] = p1['departafter']
+    fisrt_step[:arrivetime] = fisrt_step[:departtime] + legs[0]["duration"]["value"]
+    routes << first_step
+    for leg in result["legs"]
       step = {}
       step[:geocode] = geocode_to_s(leg["end_location"])
-      step[:departure] = route1[:steps].last[:arrival]
-      step[:arrival] = step[:departure] + leg["duration"]["value"]
-      route1[:steps] << step
+      step[:departtime] = route.last[:arrivetime]
+      step[:arrivetime] = step[:departtime] + leg["duration"]["value"]
+      routes << step
     end
     # order = result['routes'][0]['waypoint_order']
     # ordered_loc = order.map{|x| locations[x]}
     # ordered_loc = (@start+ordered_loc+@dest).map{|x| x['geocode']}
     totaltime = result['routes'][0]['legs'].map{|x| x['duration']['value']}.inject(:+)
-    return {errCode: SUCCESS, routes: [route1], duration: [totaltime]}
+    return {errCode: SUCCESS, route: [routes], duration: [totaltime], mode: @mode}
   end
 
 
@@ -153,126 +144,18 @@ module RoutesHelper
 
 
 
-  
-
-  # Get @intervals for each two consectutive locs in ARRANDED. Return ALL @intervals 
-  # and ERRCODE. If ERRCODE == SUCCESS, @INTERVALS is valid in that it pass 
-  # check_time_validity. 
-  def get_intervals_and_check_validity
-    puts '===============inside get_intervals_and_check_validity======================'
-    @intervals = []
-    pp @arranged
-    for i in (0..@arranged.length - 2)
-      if @arranged[i]['departafter'] > @arranged[i+1]['arrivebefore']
-        pp @arranged[i]['departafter']
-        pp @arranged[i+1]['arrivebefore']
-        @err = ERR_INVALID_INPUT_TIME
-        return
-      else
-        @intervals << @arranged[i+1]['arrivebefore'] - @arranged[i]['departafter']
-      end
-    end 
-    @err = check_time_validity
-  end
-
-  # @INTERVALS is a list of time in second. @ARRANGED is list of locations. Return 
-  # SUCCESS iff interval i is long enough to travel from loc i to loc i+1.
-  def check_time_validity
-    pp '============check_time_validity====================='
-    for i in (0..@arranged.length - 2)
-      pp @arranged[i]
-      pp @arranged[i+1]
-      if get_time([@arranged[i],@arranged[i+1]]) > @intervals[i]
-      	pp '==============interval length==========='
-      	pp @intervals
-      	pp '=========cost time============='
-      	pp get_time([@arranged[i],@arranged[i+1]])
-        return ERR_NOT_ENOUGH_TIME_FOR_TRAVEL
-      end
-    end
-    return SUCCESS
-  end
-
   def get_time(locs)
     pp '============= inside get time =========================='
     result = shortest_path(locs)
-    pp '		=============result==========='
-    pp result
     if result[:errCode] == SUCCESS 
+        pp '=============result==========='
+        pp result
         return result[:duration][0]
     else
         return Float::INFINITY
     end
   end
-
-  def request_route(locs)
-    pp '=============inside request route========='
-
-    addr = 'http://maps.googleapis.com/maps/api/directions/json?'
-    origin = 'origin=%s&' % geocode_to_s(locs.first['geocode']) 
-    dest = 'destination=%s' % geocode_to_s(locs.last['geocode'])
-    waypoints = ''
-    if locs.length >= 3
-    	waypoints = '&waypoints=optimize:true%s'
-    	for i in (2..locs.length - 2)
-      	waypoints += '|' + geocode_to_s(loc[i]['geocode'])
-    	end
-    end   
-    sensor = "&sensor=false"
-    mode = '&mode=%s' % @mode
-    addr = URI.encode(addr+origin+dest+waypoints+sensor+mode)
-    pp addr
-    require 'net/http'
-    return Net::HTTP.get(URI.parse(addr))
-  end
-
-  ## requires @start['departafter'] and end.@startbefore
-  def classify_loc
-    pp '===============Classify loc==================='
-    @arranged,@unarranged, @fuzzy = [],[], []
-    for point in @inp['locationList']
-    	preprocess(point)
-    	if point["arrivebefore"]
-      	@arranged << point
-    	else
-      	@unarranged << point
-    	end
-    	if point["geocode"] == nil
-      	@fuzzy << point
-    	end
-    end
-    @arranged.sort_by!{|x| x["arrivebefore"]}
-    pp '=====first===='
-    pp @arranged.first 
-    pp @start 
-    pp '=====last===='
-    pp @arranged.last
-    pp @dest
-    
-    if @arranged.first != @start or @arranged.last != @dest
-      @err = ERR_IN_SPECIFY_START_TIME_AND_ARRIVE_TIME
-    else
-	  	@err = SUCCESS
-		end
-		pp @err
- 	end
-
-
-  def preprocess(point)
-    puts '==================preprocess==========================='
-    pp point
-    if (point['arrivebefore'] and point['departafter']) or \
-     ((not point['arrivebefore']) and (not point['departafter']))
-      return
-    end
-
-    if (not point['departafter']) and point['arrivebefore']
-      point['departafter'] = point['arrivebefore'] + point['minduration']
-    elsif (not point['arrivebefore']) and point['departafter']
-      point['arrivebefore'] = point['departafter'] - point['minduration']
-    end
-  end
-
+  
   def partition(i, num)
     if num <= 1
       indicator = ''
@@ -296,7 +179,101 @@ module RoutesHelper
     pp result
     return result
   end
-  
+
+  # Get @intervals for each two consectutive locs in ARRANDED. Return ALL @intervals 
+  # and ERRCODE. If ERRCODE == SUCCESS, @INTERVALS is valid in that it pass 
+  # check_time_validity. 
+  def get_intervals_and_check_validity
+    @intervals = []
+    pp @arranged
+    for i in (0..@arranged.length - 2)
+      if @arranged[i]['departafter'] > @arranged[i+1]['arrivebefore']
+        puts '===============HAHA======================'
+        pp @arranged[i]['departafter']
+        pp @arranged[i+1]['arrivebefore']
+        @err = ERR_INVALID_INPUT_TIME
+        return
+      else
+        @intervals << @arranged[i+1]['arrivebefore'] - @arranged[i]['departafter']
+      end
+    end 
+    @err = check_time_validity(@intervals, @arranged)
+  end
+
+  # @INTERVALS are a list of Time. @ARRANGED is list of locations. Return 
+  # SUCCESS iff interval i is long enough to travel from loc i to loc i+1.
+  def check_time_validity
+    pp '============check_time_validity====================='
+    for i in (0..@arranged.length - 2)
+      pp @arranged[i]
+      pp @arranged[i+1]
+      if get_time(@arranged[i], [], @arranged[i+1]) > @intervals[i]
+        return ERR_NOT_ENOUGH_TIME_FOR_TRAVEL
+      end
+    end
+    return SUCCESS
+  end
+
+
+  def request_route(locs)
+    pp '=============inside request route========='
+
+    addr = 'http://maps.googleapis.com/maps/api/directions/json?'
+    origin = 'origin=%s&' % geocode_to_s(locs.first['geocode']) 
+    dest = 'destination=%s' % geocode_to_s(locs.last['geocode'])
+    places = ''
+    locations.each do |point|
+      places += '|' + geocode_to_s(point['geocode'])
+    end   
+    waypoints = '&waypoints=optimize:true%s&sensor=false' % places
+    mode = '&mode=%s' % @mode
+    addr = URI.encode(addr+origin+dest+waypoints+mode)
+
+    require 'net/http'
+    return Net::HTTP.get(URI.parse(addr))
+  end
+
+  ## requires @start['departafter'] and end.@startbefore
+  def classify_loc
+    pp '===============Classify loc==================='
+    @arranged,@unarranged, fuzzy = [],[], []
+    @inp['locationList'].each do |point|
+      preprocess(point)
+      if point["arrivebefore"]
+        @arranged << point
+      else
+        @unarranged << point
+      end
+      if point["geocode"] == nil
+        fuzzy << point
+      end
+    end
+    @arranged.sort_by!{|x| x["arrivebefore"]}
+    pp @arranged
+    
+    if @arranged != [] and (@arranged.first != @start or @arranged.last != @dest)
+      return [], [], [], ERR_NEED_SPECIFY_START_TIME_AND_ARRIVE_TIME
+    end
+
+    return @arranged, @unarranged, fuzzy, SUCCESS
+  end
+
+
+  def preprocess(point)
+    puts '============================================='
+    pp point
+    if (point['arrivebefore'] and point['departafter']) or \
+     ((not point['arrivebefore']) and (not point['departafter']))
+      return
+    end
+
+    if (not point['departafter']) and point['arrivebefore']
+      point['departafter'] = point['arrivebefore'] + point.minduration
+    elsif (not point['arrivebefore']) and point['departafter']
+      point['arrivebefore'] = point['departafter'] - point.minduration
+    end
+  end
+
   def read_time(text)
     if text == '' or text == nil
       return nil
@@ -305,12 +282,12 @@ module RoutesHelper
     minute, ap = rest[0..1], rest[2]
     hour = hour.to_i
     minute = minute.to_i
-    if ap == 'p' and hour != 12
+    if ap == 'p'
       hour += 12
     elsif ap == 'a' and hour == 12
       hour = 0
     end
-    return Time.new(@year, @month, @day, hour, minute)
+    return DateTime.new(@year, @month, @day, hour, minute)
   end
 
   def read_duration(text)
@@ -323,9 +300,7 @@ module RoutesHelper
   end
 
   def geocode_to_s(geocode)
-    pp '===========inside geocode_to_s=============='
-    pp geocode
-    return geocode['lat'].to_s + ',' + geocode['lng'].to_s
+    return geocode[:lat].to_s + ',' + geocode[:lng].to_s
   end 
 
   def general_search
