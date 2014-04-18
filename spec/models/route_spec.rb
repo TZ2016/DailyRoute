e15802bcb86117d8c8205b9c0ca2713282d55d73
Qi <gzzhongqi@gmail.com>
2014-04-18 01:35:48 -0700

    added some tests

---
+++ spec/models/route_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe User do
+
+	let(:user) {User.create(email: "test@test.com", password: "password", password_confirmation: "password")}
+	before do
+
+		@route = Route.new(user_id: user.id, name: "test_route", mode: "driving")
+	end
+
+	subject{@route}
+
+	it {should be_valid}
+	it {should respond_to(:user_id)}
+	it {should respond_to(:name)}
+	it {should respond_to(:mode)}
+
+	describe "mode is driving" do
+		before { @route.mode = "driving" }
+		it {should be_valid}
+	end
+	
+	describe "mode is walking" do
+		before { @route.mode = "walking" }
+		it {should be_valid}
+	end
+
+	describe "mode is transit" do
+		before { @route.mode = "transit" }
+		it {should be_valid}
+	end	
+
+	describe "mode is bicycling" do
+		before { @route.mode = "bicycling" }
+		it {should be_valid}
+	end
+
+	describe "mode is not included" do
+		before { @route.mode = "bilibili" }
+		it {should_not be_valid}
+	end	
+
+	describe "mode not present" do
+		before { @route.mode = nil }
+		it {should_not be_valid}
+	end
+
+	describe "mode is in uppercase" do
+		before { @route.mode = "Bicycling" }
+		it {should be_valid}
+	end
+
+	after(:each) do
+  		User.destroy_all
+  		Route.destroy_all
+	end
+end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -1,5 +1,66 @@
 require 'spec_helper'
 
 describe Step do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+	user = User.create(email: "test@test.com", password: "password", password_confirmation: "password")
+	route = Route.create(user_id: user.id, name: "test_route", mode: "driving")
+	before do
+		@step = Step.new(route_id: route.id, name: "test_step", geocode: "88.88, 88.88", departure: DateTime.now, arrival: DateTime.now)
+	end
+
+	subject{@route}
+	it {should be_valid}
+
+	it {should response_to(route_id)}
+	it {should response_to(name)}
+	it {should response_to(geocode)}
+	it {should response_to(departure)}
+	it {should response_to(arrival)}
+
+	describe "route_id not exist" do
+		before {@step.route_id = nil}
+		it {should_not be_valid}
+	end
+
+	describe "name not exist" do
+		before {@step.name = nil}
+		it {should be_valid}
+	end	
+
+	describe "geocode not exist" do
+		before {@step.geocode = nil}
+		it {should_not be_valid}
+	end
+
+	describe "geocode wrong format" do
+		before {@step.geocode = "23"}
+		it {should_not be_valid}
+	end	
+
+	describe "geocode wrong format" do
+		before {@step.geocode = "23, 23"}
+		it {should_not be_valid}
+	end	
+
+	describe "geocode wrong format" do
+		before {@step.geocode = "21.13"}
+		it {should_not be_valid}
+	end	
+
+	describe "arrival not exist" do
+		before {@step.arrival = nil}
+		it {should_not be_valid}
+	end
+
+	describe "departure not exist" do
+		before {@step.departure = nil}
+		it {should_not be_valid}
+	end
+
+
+	after(:each) do
+		User.destroy_all
+		Route.destroy_all
+		Step.destroy_all
+	end
 end
