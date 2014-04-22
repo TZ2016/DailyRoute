36ae282dbc2cd3f044ffef38cbfdd6cfa3ff0390
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-04-22 15:35:10 -0700

    main page tip alert

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,8 @@
 class StaticPageController < ApplicationController
 
   def root
+    flash[:warning] = 'Beta version: You must log in before creating any route!'
+    flash[:info] = 'Tip: The first location on your list is considered as the start, and the last the end.'
     redirect_to :main
   end
 
