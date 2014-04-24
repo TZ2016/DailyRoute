427b7845f8b367ed00b2a19aab11640f879b8995
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-04-24 11:06:33 -0700

    before ajaxify calcroute button

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,14 +1,14 @@
 class RoutesController < ApplicationController
 
   before_action :signed_in_user, only: [:destroy]
-  before_action :correct_route,  only: [:destroy]
+  before_action :correct_route, only: [:destroy]
 
 
   # create a list of routes from a single request
   def create
-    @routes = []
+    @routes   = []
     @messages = []
-  	result = solve(route_params)
+    result    = solve(route_params)
     if result[:errCode] == 1
       if build_routes(result[:routes])
         flash.now[:success] = 'Route created!'
@@ -20,7 +20,7 @@ class RoutesController < ApplicationController
         flash.now[:error] = 'Route generated but encounter errors while saving'
         respond_to do |format|
           format.html { redirect_to root_url } #FIXME
-          format.json { render :json => result }
+          format.json { render :json => { errCode: -100 } }
         end
       end
     else
@@ -31,9 +31,9 @@ class RoutesController < ApplicationController
       end
     end
   end
-  
+
   # to show a view on a route (depend on @route)
-  def show 
+  def show
   end
 
   # to show a list of routes (depend on @routes)
@@ -60,39 +60,42 @@ class RoutesController < ApplicationController
   def destroy
     flash[:success] = 'Route:' + @route.name + '(id=' + @route.id.to_s + ') is deleted.'
     @route.destroy
-    redirect_to current_user
+    respond_to do |format|
+      format.html { redirect_to current_user }
+      format.js #FIXME
+    end
   end
 
   private
 
-    def route_params
-      params.require(:route).permit! #FIXME
-    end
-  
-    def correct_route
-      @route = current_user.routes.find_by(id: params[:id])
-      redirect_to root_url if @route.nil?
-    end
+  def route_params
+    params.require(:route).permit! #FIXME
+  end
 
-    def build_routes(routes_list)
-      user_id = signed_in? ? current_user.id : 0
-      routes_list.each do |r|
-        route_params = { name: r[:name],
-                         mode: r[:mode],
-                         user_id: user_id,
-                         steps_attributes: [] }
-        r[:steps].each do |s|
-          route_params[:steps_attributes] << { name: s[:name],
-                                               geocode: s[:geocode],
-                                               arrival: s[:arrival],
-                                               departure: s[:departure]}
-        end
-        @routes << Route.new(route_params)  # nested attributes
+  def correct_route
+    @route = current_user.routes.find_by(id: params[:id])
+    redirect_to root_url if @route.nil?
+  end
+
+  def build_routes(routes_list)
+    user_id = signed_in? ? current_user.id : 0
+    routes_list.each do |r|
+      route_params = { :name             => r[:name],
+                       :mode             => r[:mode],
+                       :user_id          => user_id,
+                       :steps_attributes => [] }
+      r[:steps].each do |s|
+        route_params[:steps_attributes] << { :name      => s[:name],
+                                             :geocode   => s[:geocode],
+                                             :arrival   => s[:arrival],
+                                             :departure => s[:departure] }
       end
-      @routes.each do |r|
-        return false unless r.valid?
-      end
-      true
+      @routes << Route.new(route_params) # nested attributes
     end
+    @routes.each do |r|
+      return false unless r.save
+    end
+    true
+  end
 
 end
