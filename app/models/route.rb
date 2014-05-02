25edd3c0539ad2125dde494d9e084531342b5b93
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-05-02 12:02:03 -0700

    add request constraint model

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,13 +3,15 @@ class Route < ActiveRecord::Base
   before_validation { self.mode = mode.downcase }
 
   belongs_to :user
+  # belongs_to :request
   has_many :steps, inverse_of: :route, dependent: :destroy
   accepts_nested_attributes_for :steps
 
   default_scope -> { order('created_at DESC') }
 
   validates :user_id, presence: true
-  validates :mode, presence: true
+  # validates :request_id, presence: true
+  validates :mode, presence: true # FIXME: remove mode, dependent on request
   validates :mode, inclusion: %w(driving transit bicycling walking)
 
 end
